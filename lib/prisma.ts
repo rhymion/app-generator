9adbd4a8a529ce58f41153cd735adb509bbb8aec
Doreen Admin <doreen.amit.kumar@gmail.com>
2026-01-08 11:28:13 +0900

    feat: Show book information

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from '@/app/generated/prisma/client'
+// import { withAccelerate } from '@prisma/extension-accelerate';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL
+});
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const prisma =
-  globalForPrisma.prisma || new PrismaClient({ accelerateUrl: process.env.DATABASE_URL as string, log: ['query'] });
+  globalForPrisma.prisma || new PrismaClient({ adapter, log: ['query'] });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
