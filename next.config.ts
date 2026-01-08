69f1f4897f73ddb43ff2fefddd30e9c1055c7c87
Doreen Admin <doreen.amit.kumar@gmail.com>
2026-01-08 09:51:28 +0900

    Add code for book information

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'books.google.com'
+      },
+    ]
+  },
+  // devIndicators: false,
+  experimental: {
+    cacheComponents: true,
+  },
 };
 
 export default nextConfig;
