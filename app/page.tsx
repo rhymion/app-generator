087b5f33a9893d7003862d0acca58e52248e2fa6
Doreen Admin <doreen.amit.kumar@gmail.com>
2026-01-09 18:21:38 +0900

    move: Move review display component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import ReviewsClient from '@/components/ReviewsClient';
-
 export default function Home() {
   return (<h2 className='text-3xl text-gray-800 font-bold my-8'>Welcome to Reading Recorder</h2>);
 }
--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,5 +1,9 @@
-import ReviewsClient from '@/components/ReviewsClient';
+import LinkedBookDetails from '@/components/LinkedBookDetails'
+import { getAllReviews } from '@/lib/getters';
 
-export default function ReviewsPage() {
-  return <ReviewsClient />;
+export default async function ReviewsPage() {
+  const reviews = await getAllReviews();
+  return reviews.map((b, i) => (
+    <LinkedBookDetails book={b} index={i + 1} key={b.id} />
+  ));
 }
