a103c00880d9775d08b4e774dcf94f2cf06220ac
Sean <sean@doreenslab.com>
2026-02-06 17:53:30 +0900

    chore: Update style

--- app/@sidebar/page.tsx
+++ app/@sidebar/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 export default function HeaderPage() {
   return <>
-    <nav className="w-32 h-lvh bg-blue-600 flex-none">
+    <nav className="w-32 h-lvh bg-gray-200 flex-none">
       <ul className="mb-4 pl-2">
         <li className="block px-4 py-2 my-1 hover:bg-gray-100 rounded">
-          <Link className="no-underline text-blue-300" href="/">
+          <Link className="no-underline text-gray-600" href="/">
             Home</Link></li>
-        <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
+        {/* <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
           <Link className="no-underline text-blue-300" href="/db_table">
             DB Tables</Link></li>
         <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
@@ -17,18 +17,18 @@ export default function HeaderPage() {
             Parent Only</Link></li>
         <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
           <Link className="no-underline text-blue-300" href="/parent1">
-            Parent1</Link></li>
-        <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
-          <Link className="no-underline text-blue-300" href="/user_account">
+            Parent1</Link></li> */}
+        <li className="block text-gray-600 px-4 py-2 my-1 hover:bg-gray-100 rounded">
+          <Link className="no-underline text-gray-600" href="/user_account">
             User Account</Link></li>
-        <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
-          <Link className="no-underline text-blue-300" href="/role">
+        <li className="block text-gray-600 px-4 py-2 my-1 hover:bg-gray-100 rounded">
+          <Link className="no-underline text-gray-600" href="/role">
             Role</Link></li>
-        <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
-          <Link className="no-underline text-blue-300" href="/organization">
+        <li className="block text-gray-600 px-4 py-2 my-1 hover:bg-gray-100 rounded">
+          <Link className="no-underline text-gray-600" href="/organization">
             Organization</Link></li>
-        <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
-          <Link className="no-underline text-blue-300" href="/permission">
+        <li className="block text-gray-600 px-4 py-2 my-1 hover:bg-gray-100 rounded">
+          <Link className="no-underline text-gray-600" href="/permission">
             Permission</Link></li>
       </ul>
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return (<h2 className='text-3xl text-gray-800 font-bold my-8'>Welcome to Reading Recorder</h2>);
+  return (<h2 className='text-3xl text-gray-800 font-bold my-8'>Welcome to our generated app</h2>);
 }
