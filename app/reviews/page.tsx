45881d152ead83aec1b9a9cd0668076a0b4a0019
Sean <sean@doreenslab.com>
2026-02-10 17:30:59 +0900

    clean: Delete old files

--- app/reviews/page.tsx
+++
@@ -1,9 +0,0 @@
-import LinkedBookDetails from '@/components/LinkedBookDetails'
-import { getAllReviews } from '@/lib/getters';
-
-export default async function ReviewsPage() {
-  const reviews = await getAllReviews();
-  return reviews.map((b, i) => (
-    <LinkedBookDetails book={b} index={i + 1} key={b.id} />
-  ));
-}
