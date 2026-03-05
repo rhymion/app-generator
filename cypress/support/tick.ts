77028799af1b012793e5dc8110cb66e6e0cc43b3
Sean <sean@doreenslab.com>
2026-03-05 19:14:40 +0900

    test: Use fireEvent instead of userEvent to prevent error in GitHub Actions

--- cypress/support/tick.ts
+++ cypress/support/tick.ts
@@ -1,16 +1,15 @@
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen, fireEvent } from '@testing-library/react';
 
 export class TickTestUtils {
-  static async tickRows(rows: number[]) {
-    const headerCheckbox = await screen.getAllByRole('checkbox')[0];
+  static tickRows(rows: number[]) {
+    const headerCheckbox = screen.getAllByRole('checkbox')[0];
     if (headerCheckbox.getAttribute('aria-label') === 'Select all rows') {
-      await userEvent.click(headerCheckbox); // select all
+      fireEvent.click(headerCheckbox); // select all
     }
-    await userEvent.click(headerCheckbox); // Clear all selections
+    fireEvent.click(headerCheckbox); // Clear all selections
     for (const rowIndex of rows) {
-      const checkboxes = await screen.getAllByRole('checkbox');
-      await userEvent.click(checkboxes[rowIndex]);
+      const checkboxes = screen.getAllByRole('checkbox');
+      fireEvent.click(checkboxes[rowIndex]);
     }
   }
 }
